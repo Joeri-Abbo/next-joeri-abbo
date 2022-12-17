39aff309601c13e3d9dfa0be3e65e04a8046ae2e
Joeri Abbo <joeriabbo@hotmail.com>
2022-12-17 17:57:28 +0100

    Add dark mode

---
+++ sections/Layout.tsx
@@ -0,0 +1,20 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Head from "next/head";
+
+// @ts-ignore
+const Layout = ({children}) => {
+    return (
+        <>
+            <div className="min-h-screen mx-auto max-w-2xl flex flex-col">
+                <Header/>
+                <main className="flex-grow container mx-auto px-4 sm:px-6">
+                    {children}
+                </main>
+                <Footer/>
+            </div>
+        </>
+    );
+};
+
+export default Layout;
