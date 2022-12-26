7c10bcfff79c372f3e2db9c4d25f27d4a214c5b4
Joeri Abbo <joeriabbo@hotmail.com>
2022-12-26 17:35:38 +0100

    Add underline

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,14 +1,19 @@
+import Underline from "../public/underline.svg";
+
 type Props = {
     children: JSX.Element[] | JSX.Element | string
     name?: string | null
 };
 const ContentWithImage = (props: Props) => {
+    let iconClass = "w-20 7hover:scale-125 transform-gpu fill-blue-500 m-auto"
+
     return (
         <div className="text-6xl italic text-center my-8">
             `{props.children}`
             {props.name !== null &&
                 <span className="text-xs block">
                     {props.name}
+                    <Underline className={iconClass}/>
                 </span>
             }
         </div>
