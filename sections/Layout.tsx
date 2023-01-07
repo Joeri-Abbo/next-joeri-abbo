5e05440254206f5444b4145916c3ffa37e749ba9
Joeri Abbo <joeriabbo@hotmail.com>
2023-01-07 21:25:01 +0100

    Split up Layout partial

--- sections/Layout.tsx
+++ sections/Layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import ScrollableAnchor from "../components/Utilities/ScrollableAnchor";
+import Main from "../components/Sections/Layout/Main";
 
 type Props = {
     children: JSX.Element[] | JSX.Element | string
@@ -9,19 +10,10 @@ const Layout = (props: Props) => {
     return (
         <>
             <ScrollableAnchor id="top"/>
-
             <Header/>
-            <div className="min-h-screen mx-auto max-w-6xl flex flex-col">
-                <main className="flex-grow container mx-auto px-4 sm:px-6 mt-6">
-                    {props.children}
-                </main>
-            </div>
-
-            {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-            {/*<script src="https://unpkg.com/magic-snowflakes/dist/snowflakes.min.js"></script>*/}
-            {/*<script>*/}
-            {/*    new Snowflakes();*/}
-            {/*</script>*/}
+            <Main>
+                {props.children}
+            </Main>
             <Footer/>
         </>
     );
