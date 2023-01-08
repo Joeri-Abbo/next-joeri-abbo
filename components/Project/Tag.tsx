db71d0e724fd981129c1dc689d974087b1684f0f
Joeri Abbo <joeriabbo@hotmail.com>
2023-01-08 11:26:49 +0100

    Remove unused imports.

--- components/Project/Tag.tsx
+++ components/Project/Tag.tsx
@@ -1,11 +1,8 @@
-import React, {useId} from 'react'
-
 type Tag = {
     children: JSX.Element[] | JSX.Element | string
 }
 
 const Project = (tag: Tag) => {
-    const id = useId();
 
     return (
         <div>
