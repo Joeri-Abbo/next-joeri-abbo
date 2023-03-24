77882bdd539a563a43d01c5acbe7f85049429a29
Joeri Abbo <joeriabbo@hotmail.com>
2023-03-24 22:54:48 +0100

    Logo and favicon

--- components/Sections/Header/Logo.tsx
+++ components/Sections/Header/Logo.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Logo = () => {
     return (
-        <Link href="/" className="my-2 flex items-center space-x-1 text-blue-500">
-            <span className="font-bold text-3xl font-sans tracking-tight whitespace-nowrap">
+        <Link href="/" className=" flex items-center space-x-1 text-blue-500">
+            <Image src="/hero.jpeg" className="rounded-full object-cover h-12 w-12 object-left mr-4"
+                   alt="Me"
+                   width="50" height="50"/>
+            <span className="font-bold text-3xl font-sans tracking-tight whitespace-nowrap my-2 ml-8">
                 Joeri Abbo
             </span>
+
         </Link>
     )
 }
