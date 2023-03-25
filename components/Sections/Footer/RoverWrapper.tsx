d741f9a871ab8d5995b3a68ac2d0c99824d9790e
Joeri Abbo <joeriabbo@hotmail.com>
2023-03-25 20:46:32 +0100

    Refactor components

--- components/Sections/Footer/RoverWrapper.tsx
+++ components/Sections/Footer/RoverWrapper.tsx
@@ -5,7 +5,8 @@ const RoverWrapper = () => {
     return (
         <div className="relative h-8 overflow-hidden p-0">
             <div>
-                <MarsRover className={styles.mars_rover + " w-8 h-8 fill-blue-500 dark:fill-white absolute"}/>
+                <MarsRover
+                    className={styles.mars_rover + " animate-duration-30s animate-delay-2s animate-iteration-continuous animate-ease-in-out repeat-infinite left-[50px] absolute w-8 h-8 fill-blue-500 dark:fill-white absolute"}/>
             </div>
         </div>
     )
